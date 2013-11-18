15e458fa1a66792a111cde33d796b4635b46cbd8
Aidan Hutchinson-Chee <aidan_hutch@hotmail.com>
2013-11-17 23:51:31 -0500

    Removed database info oops.

--- mobile/connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-define('DB_HOST','webdesign4.georgianc.on.ca');
-define('DB_USER','db200239580');
-define('DB_PASSWORD','27606');
-define('DB_NAME','db200239580');
-?>
